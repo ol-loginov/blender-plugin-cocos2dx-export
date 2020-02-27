a7975c398b26a0644e759e9ef01d8556d40247f4
Oleg Loginov <ol.loginov@gmail.com>
2020-02-27 20:40:06 +0300

    first

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# blender-plugin-cocos2dx-export
+https://blender.stackexchange.com/questions/12147/debug-and-step-through-addon
+https://www.youtube.com/watch?v=mYrPqrFY7mA
+https://github.com/sybrenstuvel/random-blender-addons/blob/master/remote_debugger.py
---
+++ __init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+bl_info = {
+    "name": "Cocos2d-x exporter",
+    "author": "ol-loginov@gmail.com",
+    "version": (0, 1, 0),
+    "blender": (2, 80, 0),
+    "location": "File > Export",
+    "description": "Exports objects to Cocos2d-x",
+    "category": "Import-Export"
+}
+
+
+def menu_func_export(self, context):
+    pass
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
+
+
+if __name__ == '__main__':
+    register()
